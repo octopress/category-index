092452511b1b5d37153fa96d275fa7aef46d2352
Brandon Mathis <brandon@imathis.com>
2015-02-16 00:10:31 -0600

    Added support for multilingaul configurations

--- lib/octopress-categories.rb
+++ lib/octopress-categories.rb
@@ -1,63 +1,69 @@
 require 'octopress-ink'
 require 'octopress-categories/version'
-require 'octopress-categories/page_asset'
+#require 'octopress-categories/page_asset'
 require 'octopress-categories/filters'
 
 module Octopress
   module Categories
+    # Simple Page class override
+    class CategoryPage < Jekyll::Page
+      attr_accessor :dir, :name
+      include Ink::Convertible
+      
+      def relative_asset_path
+        site_source = Pathname.new Octopress.site.source
+        page_source = Pathname.new @base
+        page_source.relative_path_from(site_source).to_s
+      end
+    end
+    
     class Plugin < Ink::Plugin
 
       attr_reader :category_pages
 
-      def initialize(options)
-        super
-        @category_pages = []
-      end
-
       def register
         super
-        unless @assets_path.nil?
-          if Octopress::Ink.enabled?
-            add_category_pages
-          end
+        if Octopress::Ink.enabled?
+          add_category_pages
         end
-      end
-
-      def assets
-        superAssets = super
-        superAssets.merge!({
-                             "category-pages" => @category_pages
-        })
       end
 
       def add_category_pages
 
         # Find the correct template
-        template = @includes.detect { |l|
+        template = @includes.find { |l|
           l.file == "category_index.html"
         }
 
         created_pages = []
-        if defined? Octopress::Multilingual
+        if defined?(Octopress::Multilingual) && Octopress.site.config['lang']
           # We need to create pages for each language
           Octopress.site.languages.each do |lang|
-            category_pages_lang = createCategoryPages(template, config(lang))
-            category_pages_lang.each { |p|
-              p.data.merge!({"lang"=>lang})
-            }
+            category_pages_lang = createCategoryPages(template, lang)
             created_pages.concat(category_pages_lang)
           end
         else
-          created_pages = createCategoryPages(template, config)
+          created_pages = createCategoryPages(template)
         end
+        
 
-        @category_pages.concat(created_pages)
+        Octopress.site.pages.concat(created_pages)
       end
 
-      def createCategoryPages(template, config)
+      def createCategoryPages(template, lang=nil)
         categoryPages = []
-        Octopress.site.categories.keys.each do |category|
-          categoryPages << CategoryPageAsset.new(self, template, category)
+        c = config(lang)
+        categories = c['categories'] || Octopress.site.categories.keys
+        categories.each do |category|
+          page = CategoryPage.new(Octopress.site, File.dirname(template.path), '.', File.basename(template.path))
+          page.data['category'] = category
+          page.data['layout'] = config(lang)['layout']
+          page.dir = category_dir(category, lang)
+          page.data['lang'] = lang
+          page.data['title'] = config(lang)['title'] + category
+          page.process('index.html')
+
+          categoryPages << page
         end
         categoryPages
       end
@@ -67,11 +73,9 @@ module Octopress
         super
       end
 
-      def category_dir(category)
-        category_base_dir = config['category_dir'].gsub(/^\/*(.*)\/*$/, '\1')
-        # Make sure the category directory begins with a slash.
-        category_base_dir = "/#{category_base_dir}" unless category_base_dir =~ /^\//
-        File.join(category_base_dir, slugifyCategory(category))
+      def category_dir(category, lang=nil)
+        category_base_dir = config(lang)['dir'].gsub(/^\/*(.*)\/*$/, '\1')
+        File.join('/', lang || '', category_base_dir, slugifyCategory(category))
       end
 
       def slugifyCategory(category)
@@ -81,15 +85,14 @@ module Octopress
   end
 end
 
-#Octopress::Ink.add_plugin({
-Octopress::Ink::Plugins.register_plugin(Octopress::Categories::Plugin,{
-                                          name:          "Octopress Categories",
-                                          slug:          "categories",
-                                          gem:           "octopress-categories",
-                                          path:          File.expand_path(File.join(File.dirname(__FILE__), "..")),
-                                          type:          "plugin",
-                                          version:       Octopress::Categories::VERSION,
-                                          description:   "Category pages for Octopress and Jekyll pages.",                                # What does your theme/plugin do?
-                                          source_url:    "https://github.com/drallgood/octopress-categories",
-                                          website:       "https://github.com/drallgood/octopress-categories"                                 # Optional project website
+Octopress::Ink::Plugins.register_plugin(Octopress::Categories::Plugin, {
+  name:          "Octopress Categories",
+  slug:          "categories",
+  gem:           "octopress-categories",
+  path:          File.expand_path(File.join(File.dirname(__FILE__), "..")),
+  type:          "plugin",
+  version:       Octopress::Categories::VERSION,
+  description:   "Category pages for Octopress and Jekyll pages.",
+  source_url:    "https://github.com/drallgood/octopress-categories",
+  website:       "https://github.com/drallgood/octopress-categories"
 })
